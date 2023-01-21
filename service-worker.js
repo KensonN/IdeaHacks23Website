db6e2b694d1e1b2e690e6574f5bbbdc78b90bec1
KensonN <kenson.nguyen3@gmail.com>
2023-01-21 10:57:15 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ideahacks23/precache-manifest.23dada82df4a5595a3628001b2ab9ee2.js"
+  "/IdeaHacks23Website/precache-manifest.673a3129516a2ba2925e6787308ae7b8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ideahacks23/index.html", {
+workbox.routing.registerNavigationRoute("/IdeaHacks23Website/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
